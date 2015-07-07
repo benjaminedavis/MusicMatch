8a0ab8772f5cf471a7f8b0feedc3d650853108d2
Ben Davis <benjaminedavis@gmail.com>
2015-07-06 17:44:33 -0700

    second commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var album1 = document.querySelector("#album1");
+var album2 = document.querySelector("#album2");
+var album3 = document.querySelector("#album3");
+var album4 = document.querySelector("#album4");
+var startbutton = document.querySelector("#startbutton");
+
+album1.addEventListener('click', function selectAlbum);
+album2.addEventListener('click', );
+album3.addEventListener('click', );
+album4.addEventListener('click', );
+startbutton.addEventListener('click', );
+
+
+
+var player1Score =
+var player2Score =
+function getWinner ();
+
+if (player1Score > player2Score){
+  console.log("Player 1 Wins!");
+} else if (player1Score < player2Score){
+  console.log("Player 1 Wins!");
+}
+  else {
+    console.log(It's A Tie!);
+  }
