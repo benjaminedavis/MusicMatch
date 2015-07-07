7d7eb635c44ac17c6cc32ffffccd8741a0d81cd2
Ben Davis <benjaminedavis@gmail.com>
2015-07-07 07:52:07 -0700

    third commit

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-var album1 = document.querySelector("#album1");
-var album2 = document.querySelector("#album2");
-var album3 = document.querySelector("#album3");
-var album4 = document.querySelector("#album4");
+var song1Id = https://www.youtube.com/watch?v=-crgQGdpZR0;
+var album1Id = https://upload.wikimedia.org/wikipedia/en/2/27/ABBA_-_The_Album_%28Polar%29.jpg;
+
+var song2Id = https://www.youtube.com/watch?v=3fIqq5XVFKQ;
+var album2Id = https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg;
+
+var song3Id = https://www.youtube.com/watch?v=mrZRURcb1cM;
+var album3Id = https://upload.wikimedia.org/wikipedia/en/f/fb/FMacRumours.PNG;
+
+var song4Id = https://www.youtube.com/watch?v=XTUi9l84fRw;
+var album4Id = https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_-_Abbey_Road.jpg;
+
+var albumpos1 = document.querySelector("#album1");
+var albumpos2 = document.querySelector("#album2");
+var albumpos3 = document.querySelector("#album3");
+var albumpos4 = document.querySelector("#album4");
 
 var startbutton = document.querySelector("#startbutton");
 
 var roundCounter = 1;
-
 var playerCount = 1;
-
 var playerScore1 = 0;
 var playerScore2 = 0;
 
-
-document.getElementById("myBtn").addEventListener("click", function(){
-    document.getElementById("demo").innerHTML = "You won!";
-});
-
-
-
+// document.getElementById("myBtn").addEventListener("click", function(){
+//     document.getElementById("demo").innerHTML = "You won!";
+// });
 // function hideInstructions();
 // function hideStartButton();
 // function startSong();
@@ -44,4 +50,4 @@ function getWinner() {
 }   else {
     console.log(It's A Tie!);
   }
-};
+}
